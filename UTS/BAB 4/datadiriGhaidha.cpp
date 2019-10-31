1f038eb5e36be572b5e4a8c5a1ad36958d9ce872
ghaidhaayu <56374922+ghaidhaayu@users.noreply.github.com>
2019-10-31 11:01:27 +0700

    Add files via upload

---
+++ UTS/BAB 4/datadiriGhaidha.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout <<" =========================================================\n";
+    cout <<" Nama            : Ghaidha Ayu Wulandari                  \n";
+    cout <<" Tanggal lahir   : 26  Mei 2001                           \n";
+    cout <<" No HP           : 081394026622                           \n";
+    char tampilkan [1];
+    char nama [50], lahir[50], alamat [50], nohp [50];
+    cout << "Nama : ";
+    cin.getline(nama,50);
+    cout << "Tanggal Lahir :";
+    cin.getline(lahir,50);
+    cout << "Alamat :";
+    cin.getline(alamat,50);
+    cout << "NO HP :";
+    cin. getline (nohp, 50);
+    cout<<endl;
+    cout<<nama<<endl;
+    cout<<lahir<<endl;
+    cout<<alamat<<endl;
+    cout<<nohp<<endl;
+
+}
