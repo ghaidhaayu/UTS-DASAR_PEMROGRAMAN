30b7b27ffef78aae1fa2711a1448abe9611bd32f
ghaidhaayu <ghaidhaayu26@gmail.com>
2019-10-31 13:39:53 +0700

    UTS-DASAR-PEMROGRAMAN

---
+++ BAB 1/coba.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout << "Selamat mencoba Program C++." << endl;
+    cout << "Suksees Selalu!!" << endl;
+
+    return 0;
+}
---
+++ BAB 6/pengulangan do while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int pencacah = 1;
+    do
+    {
+        cout << " Teknik Informatika STMIK Sumedang" << endl;
+        cout << " Pemrograman C++ pengulangan do...while" << endl << endl;
+
+    }while (pencacah) <=10 ;
+
+    return 0;
+}
